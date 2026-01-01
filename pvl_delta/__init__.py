122a708bc066e629aeb45014f8e557d33d03d40f
Márton KardosØ <power.up1163@gmail.com>
2026-01-01 19:33:08 +0100

    Added utility function to module

--- pvl_delta/__init__.py
+++ pvl_delta/__init__.py
@@ -1,3 +1,3 @@
-from pvl_delta.model import pvl_delta_model, trace_qs
+from pvl_delta.model import pvl_delta_model, trace_qs, utility_function
 
-__all__ = ["pvl_delta_model", "trace_qs"]
+__all__ = ["pvl_delta_model", "trace_qs", "utility_function"]
